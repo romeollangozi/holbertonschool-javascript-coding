bc4d4da0392f80b54d0e287e55bd142473dce00a
romeollangozi <romeollangozi123@gmail.com>
2024-01-22 12:33:09 +0100

    s

--- javascript-web_scraping/2-statuscode.js
+++ javascript-web_scraping/2-statuscode.js
@@ -1,10 +1,9 @@
 #!/usr/bin/node
-const request = require('request');
+const request = require('request')
 
 request(process.argv[2], (error, response, body) => {
   if (error) {
     console.log(error);
-    return;
   }
   console.log('code: ', response && response.statusCode);
 });
