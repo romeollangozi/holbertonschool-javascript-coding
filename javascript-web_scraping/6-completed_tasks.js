bbb44a5d9b955f01f51937c3a8ba6860b54f3114
romeollangozi <romeollangozi123@gmail.com>
2024-01-23 01:18:12 +0100

    s

--- javascript-web_scraping/6-completed_tasks.js
+++ javascript-web_scraping/6-completed_tasks.js
@@ -1,26 +1,23 @@
 #!/usr/bin/node
 const request = require('request');
 
-request(
-  process.argv[2],
-  (error, request, body) => {
-    if (error) {
-      console.log(error);
-    }
-
-    const res = JSON.parse(body);
-    const completedTasks = {};
-
-    res.map((ele) => {
-      if (ele.completed) {
-        if (ele.userId in completedTasks) {
-          completedTasks[ele.userId] += 1;
-        } else {
-          completedTasks[ele.userId] = 1;
-        }
-      }
-    });
-
-    console.log(completedTasks);
+request(process.argv[2], (error, request, body) => {
+  if (error) {
+    console.log(error);
   }
-);
+
+  const res = JSON.parse(body);
+  const completedTasks = {};
+
+  res.map((ele) => {
+    if (ele.completed) {
+      if (ele.userId in completedTasks) {
+        completedTasks[ele.userId] += 1;
+      } else {
+        completedTasks[ele.userId] = 1;
+      }
+    }
+  });
+
+  console.log(completedTasks);
+});
