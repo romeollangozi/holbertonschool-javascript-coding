00fb66beafdecdce734859fdf2f6329848d0d617
romeollangozi <109810675+romeollangozi@users.noreply.github.com>
2024-01-24 19:04:35 +0100

    Update 4-starwars_count.js

--- javascript-web_scraping/4-starwars_count.js
+++ javascript-web_scraping/4-starwars_count.js
@@ -1,13 +1,21 @@
 #!/usr/bin/node
+/*eslint-disable*/
+const process = require('process');
 const request = require('request');
+const api = process.argv[2];
 
-request(process.argv[2], (error, response, body) => {
+request(api, function (error, response, body) {
   if (error) {
-    console.log(error);
+    console.error('error:', error); // Print the error if one occurred
   }
-  const res = JSON.parse(body);
-  const newData = res.results.filter((ele) => {
-    return ele.characters.includes('https://swapi-api.hbtn.io/api/people/18/');
+  const films = JSON.parse(response.body); // Print the response status code if a response was received
+  const filtered = films.results.filter(film => {
+    for (const character of film.characters) {
+      if (character.includes('18')) {
+        return true;
+      }
+    }
+    return false;
   });
-  console.log(newData.length);
+  console.log(filtered.length);
 });
