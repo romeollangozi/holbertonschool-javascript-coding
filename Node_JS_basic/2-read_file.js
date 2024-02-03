0942defaf25c01a7dd25dca3c08a3c37dfa9fade
romeollangozi <romeollangozi123@gmail.com>
2024-02-03 01:31:49 +0100

    s

--- Node_JS_basic/2-read_file.js
+++ Node_JS_basic/2-read_file.js
@@ -11,15 +11,17 @@ const countStudents = (path) => {
     }
     const arr = data.split('\n');
     arr.shift();
+    const purifiedData = [];
     for (let i = 0; i < arr.length; i += 1) {
-      if (arr[i].length === 0) {
-        arr.splice(i, 1);
-      }
       arr[i] = arr[i].split(',');
+      if (arr[i].length === 4) {
+        purifiedData.push(arr[i]);
+      }
     }
-    stats.total_students = arr.length;
 
-    for (const ele of arr) {
+    stats.total_students = purifiedData.length;
+
+    for (const ele of purifiedData) {
       if (ele[3] in stats.fields) {
         stats.fields[ele[3]].number += 1;
         stats.fields[ele[3]].students.push(ele[0]);
@@ -33,7 +35,7 @@ const countStudents = (path) => {
       console.log(
         `Number of students in ${field.name}: ${
           field.number
-        }. List: ${field.students.join(', ')})`,
+        }. List: ${field.students.join(', ')}`,
       );
     }
   });
