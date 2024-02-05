5d6a627eb6512e16bee642092e9a5c41771e7592
romeollangozi <romeollangozi123@gmail.com>
2024-02-05 17:38:29 +0100

    s

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,18 +1,18 @@
-const http = require("http");
-const url = require("url");
-const countStudents = require("./3-read_file_async");
+const http = require('http');
+const url = require('url');
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer(async (req, res) => {
   const parsedUrl = url.parse(req.url, true);
 
-  if (parsedUrl.pathname === "/") {
-    // For the root path ('/'), display "Hello Holberton School!"
-    res.end("Hello Holberton School!");
-  } else if (parsedUrl.pathname === "/students") {
+  if (parsedUrl.pathname === '/') {
+    // For the root path ('/'), display 'Hello Holberton School!'
+    res.end('Hello Holberton School!');
+  } else if (parsedUrl.pathname === '/students') {
     try {
-      const databaseFile = process.argv[2] || "database.csv"; // // Get the database file path
+      const databaseFile = process.argv[2] || 'database.csv'; // // Get the database file path
 
-      let consoleOutput = ""; // variable to capture console.log output
+      let consoleOutput = ''; // variable to capture console.log output
       const originalConsoleLog = console.log;
       console.log = (message) => {
         consoleOutput += `${message}\n`; //  // Capture console.log output
@@ -31,9 +31,9 @@ const app = http.createServer(async (req, res) => {
       res.end(`This is the list of our students\n${error.message}`);
     }
   } else {
-    // Handle other paths with a "Not Found" response
+    // Handle other paths with a 'Not Found' response
     res.statusCode = 404;
-    res.end("Not Found\n");
+    res.end('Not Found\n');
   }
 });
 
