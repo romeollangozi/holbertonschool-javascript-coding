270a67251f7d5045b4051f3bb0f2442a424b82a7
romeollangozi <romeollangozi123@gmail.com>
2024-02-05 16:21:55 +0100

    s

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,18 +1,25 @@
-const http = require('node:http');
+const http = require('http');
+const util = require('node:util');
 const countStudents = require('./3-read_file_async');
 
+const port = 1245;
 const app = http
-  .createServer((req, res) => {
-    const { url } = req;
-    res.writeHead({ 'Content-Type': 'text/plain' });
-    if (url === '/') {
-      res.end('Hello Holberton School!');
+  .createServer(async (req, res) => {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    if (req.url === '/') {
+      res.write('Hello Holberton School!');
     }
-    if (url === '/students') {
-      const data = countStudents('database.csv');
-      res.end(data);
+    if (req.url === '/students') {
+      res.write('This is the list of our students\n');
+      try {
+        const result = await countStudents(process.argv[2]);
+        res.write(result.join('\n'));
+      } catch (error) {
+        res.write(error.message);
+      }
     }
+    res.end();
   })
-  .listen(1245);
+  .listen(port);
 
 module.exports = app;
