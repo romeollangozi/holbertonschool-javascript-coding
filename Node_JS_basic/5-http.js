6551d1c1860ea1bc254e4c9c40f976e5a017fbf6
romeollangozi <romeollangozi123@gmail.com>
2024-02-05 17:37:10 +0100

    s

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,36 +1,42 @@
 const http = require("http");
-
-const args = process.argv.slice(2);
+const url = require("url");
 const countStudents = require("./3-read_file_async");
 
-const DATABASE = args[0];
-
-const hostname = "127.0.0.1";
-const port = 1245;
-
 const app = http.createServer(async (req, res) => {
-  res.statusCode = 200;
-  res.setHeader("Content-Type", "text/plain");
+  const parsedUrl = url.parse(req.url, true);
 
-  const { url } = req;
-
-  if (url === "/") {
-    res.write("Hello Holberton School!");
-  } else if (url === "/students") {
-    res.write("This is the list of our students\n");
+  if (parsedUrl.pathname === "/") {
+    // For the root path ('/'), display "Hello Holberton School!"
+    res.end("Hello Holberton School!");
+  } else if (parsedUrl.pathname === "/students") {
     try {
-      const students = await countStudents(DATABASE);
-      res.end(`${students.join("\n")}`);
+      const databaseFile = process.argv[2] || "database.csv"; // // Get the database file path
+
+      let consoleOutput = ""; // variable to capture console.log output
+      const originalConsoleLog = console.log;
+      console.log = (message) => {
+        consoleOutput += `${message}\n`; //  // Capture console.log output
+      };
+
+      await countStudents(databaseFile); // Retrieve student data with countStudents() function
+
+      consoleOutput = consoleOutput.trim(); // // Remove extra newline from the end
+
+      console.log = originalConsoleLog; // // Restore the original console.log function
+
+      res.end(`This is the list of our students\n${consoleOutput}`);
     } catch (error) {
-      res.end(error.message);
+      // Handle errors by sending an error response
+      res.statusCode = 400;
+      res.end(`This is the list of our students\n${error.message}`);
     }
+  } else {
+    // Handle other paths with a "Not Found" response
+    res.statusCode = 404;
+    res.end("Not Found\n");
   }
-  res.statusCode = 404;
-  res.end();
 });
 
-app.listen(port, hostname, () => {
-  //   console.log(`Server running at http://${hostname}:${port}/`);
-});
+app.listen(1245); // Listen on port 1245
 
 module.exports = app;
